27aea085c70d1a665d67fa3234fbe387d5133efe
sarthak-subhash-joshi <sarthakjoshi3011@gmail.com>
2024-04-06 02:25:41 +0530

    Updating README file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import Home from './pages/Home'
 import Footer from './components/Footer'
-// import HorizontalScroller from './components/HorizontalScroller';
+
 
 const App = () => {
   return (
     <>
       <Home/>
       <Footer/>
-
-      {/* <HorizontalScroller/> */}
-
     </>
   )
 }
